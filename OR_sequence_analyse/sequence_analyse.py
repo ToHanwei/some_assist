8df272ea36b57b46f5d4bc2bf782ac69a0b4680d
ToHanwei <hanwei@shanghaitech.edu.cn>
2019-03-31 16:31:49 +0800

    2019-03-31_01

--- OR_sequence_analyse/sequence_analyse.py
+++ OR_sequence_analyse/sequence_analyse.py
@@ -7,13 +7,16 @@
 计算个位点保守程度
 """
 
+import pymysql
 import pandas as pd
 import numpy as np
 from sys import argv
 from collections import defaultdict
+from sqlalchemy import create_engine
 
 
 def count(col):
+	"""统计序列"""
 	aa_list = list("ACDEFGHIKLMNPQRSTVWY")
 	aa_dict = {a:0 for a in aa_list}
 	for aa in col:
@@ -21,23 +24,45 @@ def count(col):
 		aa_dict[aa] = aa_dict[aa] + 1
 	return aa_dict
 
-ORfile = argv[1]
-seq_dict = defaultdict(list)
+def connect_db(df):
+	"""将Count的数据储存到mysql数据库"""
+	df = df.T
+	connect_info = "mysql+pymysql://root:hanwei1@localhost:3306/Human_OR_seq"
+	engine = create_engine(connect_info)
+	df.to_sql(name="O5AN1", con=engine, if_exists='replace', index=False)
 
-with open(ORfile, mode='r') as seqfile:
-	for line in seqfile:
-		lines = line.split()
-		if len(lines) != 2: continue
-		key = lines[0].split("|")[1]
-		seq = list(lines[1])
-		if (key not in seq_dict):
-			seq_dict[key] = seq
-		else:
-			seq_dict[key].extend(seq)
-		
-seq_df = pd.DataFrame.from_dict(data=seq_dict, orient='columns')
-b = seq_df['O5AN1']!='-'
-seq_filter = seq_df[b]
-seq_filter.index = range(1, len(seq_filter)+1)
-a = seq_filter.apply(count, axis=1)
-print(pd.DataFrame.from_dict((dict(a))))
+
+def dofile():
+	ORfile = argv[1]
+	seq_dict = defaultdict(list)
+	with open(ORfile, mode='r') as seqfile:
+		for line in seqfile:
+			lines = line.split()
+			if len(lines) != 2: continue
+			key = lines[0].split("|")[1]
+			seq = list(lines[1])
+			if (key not in seq_dict):
+				seq_dict[key] = seq
+			else:
+				seq_dict[key].extend(seq)
+	seq_df = pd.DataFrame.from_dict(data=seq_dict, orient='columns')
+	seq_filter = seq_df[seq_df['O5AN1']!='-']
+	seq_filter.index = range(1, len(seq_filter)+1)
+	NUM = len(seq_filter.T)
+	acount = seq_filter.apply(count, axis=1)
+	seq_data = pd.DataFrame.from_dict((dict(acount)))*100/NUM
+	return seq_data, acount, seq_filter['O5AN1']
+
+
+def main(isave=False):
+	seq_data, acount, o5an1 = dofile()
+	if isave: connect_db(seq_data)
+	for i in acount.index:
+		seq = sorted(acount[i].items(), key=lambda x:x[1], reverse=True)
+		if seq[0][1]*100/388 >= 95:
+			print(i, o5an1[i], seq[0][0], seq[0][1]*100/388)
+
+	
+if __name__ == "__main__":
+	main()
+
